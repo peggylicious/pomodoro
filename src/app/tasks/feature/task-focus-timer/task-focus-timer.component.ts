6364da25ad976c4048e95b6fcbab5657fff15fb4
peggylicious <p.asq@live.com>
2023-05-19 19:32:12 +0100

    #21 Add base css styles

---
+++ src/app/tasks/feature/task-focus-timer/task-focus-timer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-focus-timer',
+  templateUrl: './task-focus-timer.component.html',
+  styleUrls: ['./task-focus-timer.component.scss'],
+})
+export class TaskFocusTimerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
