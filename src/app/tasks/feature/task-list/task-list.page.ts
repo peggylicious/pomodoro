f0a3112f50b311d1c32d4edcef0333ab75249a85
peggylicious <p.asq@live.com>
2023-06-01 13:29:18 +0100

    Clean-up: task-list component comments

--- src/app/tasks/feature/task-list/task-list.page.ts
+++ src/app/tasks/feature/task-list/task-list.page.ts
@@ -25,31 +25,7 @@ export class TaskListPage implements OnInit, OnDestroy {
   selectedTask:any;
   selectedIndex = undefined;
   currentRoute: any;
-  // hidePlayButton: boolean = false;
-  // isPlay: boolean = true;
-  constructor(private router: Router, private route: ActivatedRoute, private tasksStoreService: TasksStoreService, @Inject(LOCALE_ID) private locale: string) {
-    // this.router.events.subscribe((event: Event) => {
-    //   if (event instanceof NavigationStart) {
-    //       // Show progress spinner or progress bar
-    //       console.log('Route change detected', event);
-    //       history.state
-    //       this.tasksStoreService.playPomodoro(true)
-    //   }
-
-    //   if (event instanceof NavigationEnd) {
-    //       // Hide progress spinner or progress bar
-    //       this.currentRoute = event.url;
-    //       console.log(event);
-    //   }
-
-    //   if (event instanceof NavigationError) {
-    //        // Hide progress spinner or progress bar
-
-    //       // Present error to user
-    //       console.log(event.error);
-    //   }
-    // });
-  }
+  constructor(private router: Router, private route: ActivatedRoute, private tasksStoreService: TasksStoreService, @Inject(LOCALE_ID) private locale: string) {}
   ngOnInit() {
     this.route.data.subscribe((res) => {
       console.log(res)
@@ -62,37 +38,10 @@ export class TaskListPage implements OnInit, OnDestroy {
   }
 
   playPomodoro(playTime:number, task?:any){
-    // console.log(task)
-    // this.selectedTask = task.task;
-    // this.selectedIndex = task.index
-    // this.isPlay = true
-    // console.log("Play ", this.pauseTime)
-    // this.timeRemaining$ = timer(0, 1000).pipe( // 5mins = 300, 25mins = 15540
-    // map(n => {
-    //   console.log(n)
-    //   if((playTime - n) === 1){
-    //     this.pauseTime = 0;
-    //     this.timeR.next('completed')
-    //   }
-    //   this.pauseTime = playTime - n-1;
-    //   return (playTime - n) * 1000
-    // }),
-    // takeUntil(this.timeR),
-  // );
   }
   pausePomodoro(){
-    console.log("Pause,", this.pauseTime)
     this.timeR.next(this.pauseTime)
-    // this.isPlay = true
     this.tasksStoreService.updateTasks(this.selectedTask._id, {timeLeft: this.pauseTime, totalCycles: this.selectedTask.totalCycles, singleCycle: this.selectedTask.singleCycle, pomodoros: this.selectedTask.pomodoros, isComplete: this.selectedTask.pomodoro})
-    // .subscribe(res=>{
-    //   console.log(res)
-    // this.tasksStoreService.showPlayBtn(true, this.selectedIndex)
-    // // this.isPlay = true
-    //   // console.log(this.tasksStoreService.showPlayBtn.getValue())
-    // })
-    console.log("index: ", this.selectedTask.index,)
-
   }
   stopPomodoro(){
     console.log("Stop")
@@ -102,7 +51,6 @@ export class TaskListPage implements OnInit, OnDestroy {
   }
   deleteTask(){
     this.tasksStoreService.deleteAllTasks()
-
   }
   ngOnDestroy(): void {
     this.timeR.next(this.pauseTime)
